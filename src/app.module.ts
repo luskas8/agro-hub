af2a784b724d80da45808e07f5f77df085bc6620
luskas8 <lucas.anjos@franq.com.br>
2025-06-10 19:49:53 -0300

    chore: add logger & db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LoggerModule } from 'nestjs-pino';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      expandVariables: true,
+    }),
+    LoggerModule.forRoot({
+      pinoHttp: {
+        transport:
+          process.env.NODE_ENV !== 'prod'
+            ? { target: 'pino-pretty' }
+            : undefined,
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
