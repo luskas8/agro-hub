7a683bbb6d3ab046a42693763f6e351d6c055dfc
luskas8 <lucas.anjos@franq.com.br>
2025-06-12 00:29:33 -0300

    feat: add prisma globally & fix type generation folder

--- src/rural-producer/rural-producer.module.ts
+++ src/rural-producer/rural-producer.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { PrismaModule } from '../prisma/prisma.module';
 import { RuralProducerController } from './rural-producer.controller';
 import { RuralProducerService } from './rural-producer.service';
 
 @Module({
-  imports: [PrismaModule],
   controllers: [RuralProducerController],
   providers: [RuralProducerService],
 })
